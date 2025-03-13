10e4ec22db1db840f5dce3a0e149edd24c54507f
82104 <82104@DESKTOP-CEG4DJV>
2025-03-13 11:31:11 +0900

    초기 커밋.

---
+++ Project1/1완성_홍현주_5716262_0311.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void print_bit(int num) {
+    unsigned int m = 1 << 31;
+
+    for (int i = 0; i < 32; i++) {
+        if (num & m) {
+            printf("1");
+        }
+        else {
+            printf("0");
+        }
+        m >>= 1;
+    }
+}
+
+int main() {
+    int num;
+
+    printf("정수를 입력하세요: ");
+    if (scanf_s("%d", &num) != 1) {
+        printf("입력 오류\n");
+        return 1;
+    }
+
+    printf("입력된 정수의 32비트 표현: ");
+    print_bit(num);
+    printf("\n");
+
+    return 0;
+}
